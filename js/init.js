b408a60044646b37cbb523f300658518c41aa41e
nataliarosas2016 <nataliarosas2016@gmail.com>
2023-11-16 10:38:09 -0600

    update init

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyBBcxznbhKDVSbLAJXjmt4mpL-0CHd2vHE",
+    authDomain: "alumnosnatalia.firebaseapp.com",
+    projectId: "alumnosnatalia",
+    storageBucket: "alumnosnatalia.appspot.com",
+    messagingSenderId: "936432636890",
+    appId: "1:936432636890:web:a230873ee0049e3b44a6e3",
+    measurementId: "G-ZHM2NKB63C"
 });
